1d1d2d5abdece7d3e57ff8a41a4c178d5f195086
Alexey Lysenko <abesmon@gmail.com>
2022-12-14 00:59:20 +0300

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -33,6 +33,7 @@ parser.add_argument('--upsample_steps', type=int, default=64, help="num steps up
 parser.add_argument('--update_extra_interval', type=int, default=16, help="iter interval to update extra status (only valid when using --cuda_ray)")
 parser.add_argument('--max_ray_batch', type=int, default=4096, help="batch size of rays at inference to avoid OOM (only valid when not using --cuda_ray)")
 parser.add_argument('--albedo_iters', type=int, default=1000, help="training iters that only use albedo shading")
+parser.add_argument('--uniform_sphere_rate', type=float, default=0.5, help="likelihood of sampling camera location uniformly on the sphere surface area")
 # model options
 parser.add_argument('--bg_radius', type=float, default=1.4, help="if positive, use a background model at sphere(bg_radius)")
 parser.add_argument('--density_thresh', type=float, default=10, help="threshold for density grid to be occupied")
