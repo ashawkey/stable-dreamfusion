57e99bddf697138c8c40fa48d6a0b05730eb2f79
Alexey Lysenko <abesmon@gmail.com>
2022-12-14 00:55:11 +0300

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -51,6 +51,7 @@ parser.add_argument('--min_near', type=float, default=0.1, help="minimum near di
 parser.add_argument('--radius_range', type=float, nargs='*', default=[1.0, 1.5], help="training camera radius range")
 parser.add_argument('--fovy_range', type=float, nargs='*', default=[40, 70], help="training camera fovy range")
 parser.add_argument('--dir_text', action='store_true', help="direction-encode the text prompt, by appending front/side/back/overhead view")
+parser.add_argument('--suppress_face', action='store_true', help="also use negative dir text prompt.")
 parser.add_argument('--angle_overhead', type=float, default=30, help="[0, angle_overhead] is the overhead region")
 parser.add_argument('--angle_front', type=float, default=60, help="[0, angle_front] is the front region, [180, 180+angle_front] the back region, otherwise the side region.")
 
