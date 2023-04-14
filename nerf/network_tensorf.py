066db9e028951b7961b0f6420b00b59760acb326
JMackie80 <jmackie17@yahoo.com>
2023-04-14 10:20:09 -0400

    add tensorf

--- nerf/network_tensorf.py
+++ nerf/network_tensorf.py
@@ -9,6 +9,27 @@ import numpy as np
 from encoding import get_encoder
 import raymarching
 
+class MLP(nn.Module):
+    def __init__(self, dim_in, dim_out, dim_hidden, num_layers, bias=True):
+        super().__init__()
+        self.dim_in = dim_in
+        self.dim_out = dim_out
+        self.dim_hidden = dim_hidden
+        self.num_layers = num_layers
+
+        net = []
+        for l in range(num_layers):
+            net.append(nn.Linear(self.dim_in if l == 0 else self.dim_hidden, self.dim_out if l == num_layers - 1 else self.dim_hidden, bias=bias))
+
+        self.net = nn.ModuleList(net)
+    
+    def forward(self, x):
+        for l in range(self.num_layers):
+            x = self.net[l](x)
+            if l != self.num_layers - 1:
+                x = F.relu(x, inplace=True)
+        return x
+
 class NeRFNetwork(NeRFRenderer):
     def __init__(self, 
                  opt,
@@ -90,7 +111,7 @@ class NeRFNetwork(NeRFRenderer):
                 
                 bg_net.append(nn.Linear(in_dim, out_dim, bias=False))
 
-            self.bg_net = nn.ModuleList(bg_net)
+            self.bg_net = MLP(self.enc_dim_dir, 3, hidden_dim_bg, num_layers_bg, bias=True) #nn.ModuleList(bg_net)
         else:
             self.bg_net = None
 
