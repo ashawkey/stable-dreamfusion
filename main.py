b507d336be9761526251b81908ccf00566e5bc28
Vikram Voleti <vikram@ip-26-0-153-234.us-west-2.compute.internal>
2023-05-03 02:35:44 +0000

    Bugfix - WORKS!
    Former-commit-id: caa80d0c2e3dcae67ec4d55d68acdbe31713a8c8

--- main.py
+++ main.py
@@ -306,7 +306,7 @@ if __name__ == '__main__':
             gui.render()
 
         else:
-            valid_loader = NeRFDataset(opt, device=device, type='val', H=opt.H, W=opt.W, size=5).dataloader()
+            valid_loader = NeRFDataset(opt, device=device, type='val', H=opt.H, W=opt.W, size=8).dataloader()
 
             max_epoch = np.ceil(opt.iters / len(train_loader)).astype(np.int32)
             trainer.train(train_loader, valid_loader, max_epoch)
