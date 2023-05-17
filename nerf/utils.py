09a6a65bec5649dd91391506a11753251a61b701
Christian Laforte <christian.laforte@gmail.com>
2023-05-17 19:34:45 +0000

    exp_iter_ratio to cleanly keep track of experiment progress

--- nerf/utils.py
+++ nerf/utils.py
@@ -380,9 +380,13 @@ class Trainer(object):
         if do_rgbd_loss:
             data = self.default_view_data
 
+        # experiment iterations ratio
+        # i.e. what proportion of this experiment have we completed (in terms of iterations) so far?
+        exp_iter_ratio = (self.global_step - self.opt.exp_start_iter) / (self.opt.exp_end_iter - self.opt.exp_start_iter)
+
         # progressively relaxing view range
         if self.opt.progressive_view:
-            r = min(1.0, 0.2 + (self.global_step - self.opt.exp_start_iter) / (0.5 * (self.opt.exp_end_iter - self.opt.exp_start_iter)))
+            r = min(1.0, 0.2 + 2.0*exp_iter_ratio)
             self.opt.phi_range = [self.opt.default_azimuth * (1 - r) + self.opt.full_phi_range[0] * r,
                                   self.opt.default_azimuth * (1 - r) + self.opt.full_phi_range[1] * r]
             self.opt.theta_range = [self.opt.default_polar * (1 - r) + self.opt.full_theta_range[0] * r,
@@ -394,7 +398,7 @@ class Trainer(object):
 
         # progressively increase max_level
         if self.opt.progressive_level:
-            self.model.max_level = min(1.0, 0.25 + (self.global_step - self.opt.exp_start_iter) / (0.5 * (self.opt.exp_end_iter - self.opt.exp_start_iter)))
+            self.model.max_level = min(1.0, 0.25 + 2.0*exp_iter_ratio)
 
         rays_o = data['rays_o'] # [B, N, 3]
         rays_d = data['rays_d'] # [B, N, 3]
@@ -425,7 +429,7 @@ class Trainer(object):
                 rays_o = rays_o + torch.randn(3, device=self.device) * noise_scale
                 rays_d = rays_d + torch.randn(3, device=self.device) * noise_scale
 
-        elif self.global_step < (self.opt.latent_iter_ratio * self.opt.iters):
+        elif exp_iter_ratio <= self.opt.latent_iter_ratio:
             ambient_ratio = 1.0
             shading = 'normal'
             as_latent = True
@@ -433,14 +437,14 @@ class Trainer(object):
             bg_color = None
 
         else:
-            if self.global_step < (self.opt.albedo_iter_ratio * self.opt.iters):
+            if exp_iter_ratio <= self.opt.albedo_iter_ratio:
                 ambient_ratio = 1.0
                 shading = 'albedo'
             else:
                 # random shading
                 ambient_ratio = self.opt.min_ambient_ratio + (1.0-self.opt.min_ambient_ratio) * random.random()
                 rand = random.random()
-                if rand > (1.0 - self.opt.textureless_ratio):
+                if rand >= (1.0 - self.opt.textureless_ratio):
                     shading = 'textureless'
                 else:
                     shading = 'lambertian'
